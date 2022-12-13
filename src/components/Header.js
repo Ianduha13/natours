dace25cf0a4c702f5e63c6c9d58a2f2c77a06e4a
Ian Duhamel <ianduhamelhayes@gmail.com>
2022-12-13 20:28:05 -0300

    Adding header animation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,18 @@
 import "./styles/header.css"
+import logowhite from "./styles/img/logo-white.png"
 const Header = () => {
-  return <header className='header'>Some Text...</header>
+  return (
+    <header className='header'>
+      <div className='logo-box'>
+        <img src={`${logowhite}`} alt='logo' className='logo' />
+      </div>
+      <div className='text-box'>
+        <h1 className='heading-primary'>
+          <span className='heading-primary-main'>Outdoors</span>
+          <span className='heading-primary-sub'>is where life happens</span>
+        </h1>
+      </div>
+    </header>
+  )
 }
 export default Header
